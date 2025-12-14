65e1f55772a9c29e33e33cb0f91115698c085719
Angel <146152337+angel-dev21@users.noreply.github.com>
2025-12-13 22:36:16 -0500

    configure app routes and main entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router";
 import Home from "./routes/Home.tsx";
+import Products from "./routes/Products.tsx";
+import Cart from "./routes/Cart.tsx";
+import Account from "./routes/Account.tsx";
 
 function App() {
-  
-  return(
+  return (
     <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-  )
+      <Route path="/" element={<Home />} />
+      <Route path="/Products" element={<Products />} />
+      <Route path="/Cart" element={<Cart />} />
+      <Route path="/Account" element={<Account />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
