06d745f3aa8255d90cfc259ae88f70c26807bbe4
Angel <146152337+angel-dev21@users.noreply.github.com>
2025-12-21 02:32:06 -0500

    Add simple footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footbar = () => {
+  return (
+    <div className="h-20 w-full flex justify-center items-center font-bold bg-brand text-surface">
+      angel-dev21@2025
+    </div>
+  );
+};
+
+export default Footbar;
