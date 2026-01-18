942a8868deeb9f9871de4682b628575baf03f33b
angel-dev21 <146152337+angel-dev21@users.noreply.github.com>
2026-01-17 20:15:39 -0500

    Add AuthMenu component and integrate with Navbar authentication logic

---
+++ src/components/AuthMenu.tsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+import { Link } from "react-router";
+
+interface UserCardProps {
+  handleLogin: () => void;
+}
+
+const AuthMenu = ({ handleLogin }: UserCardProps) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorUser, setErrorUser] = useState(false);
+  const [errorPass, setErrorPass] = useState(false);
+  const [errorLogin, setErrorLogin] = useState(false);
+
+  const verifyFields = () => {
+    let errorDetected = false;
+
+    if (username === "" || username.length < 4 || username.length > 20) {
+      setErrorUser(true);
+      errorDetected = true;
+    }
+
+    if (password === "" || password.length < 8 || password.length > 20) {
+      setErrorPass(true);
+      errorDetected = true;
+    }
+
+    return !errorDetected;
+  };
+
+  const login = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setErrorLogin(false);
+
+    if (verifyFields()) {
+      setLoading(true);
+      fetch("http://localhost:8080/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: username, password: password }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Login failed.");
+          } else {
+            return response.text();
+          }
+        })
+        .then((data) => {
+          localStorage.setItem("jwt", data);
+          handleLogin();
+        })
+        .catch(() => setErrorLogin(true))
+        .finally(() => setLoading(false));
+    }
+  };
+
+  return (
+    <div className="w-100 absolute right-4 top-20 rounded-2xl bg-surface-dark outline-2 outline-brand-dark">
+      <h1 className="flex justify-center mt-2 text-2xl text-gray-800">
+        Log In
+      </h1>
+      <form onSubmit={login} className="flex flex-col justify-center">
+        {errorUser && (
+          <div className="bg-red-950 text-center">
+            Username must be between 4 and 20 characters.
+          </div>
+        )}
+        <input
+          type="text"
+          value={username}
+          placeholder="Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+            setErrorLogin(false);
+            if (
+              e.target.value !== "" &&
+              e.target.value.length >= 4 &&
+              e.target.value.length <= 20
+            ) {
+              setErrorUser(false);
+            }
+          }}
+          className="mx-5 mt-5 my-2 px-2 py-1 rounded-md outline-2 outline-brand-dark bg-white text-gray-800"
+        />
+        {errorPass && (
+          <div className="bg-red-950 text-center">
+            Password must be between 8 and 20 characters.
+          </div>
+        )}
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setErrorLogin(false);
+            if (
+              e.target.value !== "" &&
+              e.target.value.length >= 8 &&
+              e.target.value.length <= 20
+            ) {
+              setErrorPass(false);
+            }
+          }}
+          className="mx-5 mb-5 my-2 px-2 py-1 rounded-md outline-2 outline-brand-dark bg-white text-gray-800"
+        />
+        {loading ? (
+          <div className="text-black text-center">Loading...</div>
+        ) : (
+          <button
+            type="submit"
+            className="py-2 mx-5 my-2 rounded-2xl text-center hover:cursor-pointer outline-2 outline-brand-dark hover:bg-brand bg-brand-light text-white"
+          >
+            Sign In
+          </button>
+        )}
+      </form>
+      {errorLogin && (
+        <div className="bg-red-950 text-center">
+          Invalid username or password.
+        </div>
+      )}
+      <div className="flex justify-center">
+        <p className="py-2 mb-2 text-gray-800">
+          You don't have an account?
+          <span className="ml-1 hover:cursor-pointer text-brand">
+            <Link to={"/SignUp"}>Register here.</Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthMenu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,53 @@ import { Link } from "react-router";
 import { FaShoppingCart, FaUser, FaReact } from "react-icons/fa";
 import { IoMenu } from "react-icons/io5";
 import { AiFillProduct } from "react-icons/ai";
-import { useState } from "react";
+//import { IoMdArrowDropdown } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { useOnClickOutside } from "../hooks/useOnClickOutside";
+import AuthMenu from "./AuthMenu";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showUserCard, setShowUserCard] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const [username, setUsername] = useState("");
 
-  const ref = useOnClickOutside(() => {
+  const burgerRef = useOnClickOutside(() => {
     if (isOpen) setIsOpen(false);
   });
 
+  const userCardRef = useOnClickOutside(() => {
+    if (showUserCard) setShowUserCard(false);
+  });
+
+  const handleLogin = () => {
+    fetch("http://localhost:8080/users/username", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          setUsername(data);
+          setIsLogged(true);
+        }
+      });
+  };
+
+  useEffect(() => {
+    handleLogin();
+  }, []);
+
   return (
     <nav className="w-full h-full p-8 flex flex-row justify-between items-center bg-brand text-surface font-semibold">
-      <div className="flex flex-row gap-4 text-lg ">
+      <div className="flex flex-row items-center gap-4 text-lg ">
         <Link to="/">
           <FaReact className="hover:text-white" aria-label="Home" size={48} />
         </Link>
@@ -28,32 +62,49 @@ const Navbar = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row items-center gap-8">
-        <input
-          className="h-8 p-2 w-44 sm:w-56 bg-brand-light rounded-full focus:outline-none focus:ring-2 focus:ring-surface focus:text-surface placeholder:text-gray placeholder:w-20"
-          type="text"
-          id="product-search"
-          placeholder="Search"
-          aria-label="Search"
-        ></input>
-        <div className="sm:flex flex-row gap-8 hidden">
-          <Link to="/Cart">
-            <FaShoppingCart
-              className="hover:text-white"
-              aria-label="Cart"
-              size={32}
-            />
-          </Link>
-          <Link to="/Account">
-            <FaUser
-              className="hover:text-white"
-              aria-label="Account"
-              size={32}
-            />
-          </Link>
+      <div className="sm:flex flex-row items-center gap-4 hidden">
+        <div className="relative">
+          <input
+            className="h-8 p-2 w-44 sm:w-56 bg-brand-light rounded-full focus:outline-none focus:ring-2 focus:ring-surface focus:text-surface placeholder:text-gray placeholder:w-20 placeholder:opacity-40 placeholder:text-white"
+            type="text"
+            id="product-search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <FaSearch className="absolute bottom-2 left-49 opacity-40 text-white" />
         </div>
+        <Link to="/Cart">
+          <FaShoppingCart
+            className="hover:text-white"
+            aria-label="Cart"
+            size={32}
+          />
+        </Link>
+        {isLogged ? (
+          <div
+            className="flex items-center text-lg"
+            aria-label="View user information"
+          >
+            Hello, {username}
+            {/*<IoMdArrowDropdown size={28} />*/}
+          </div>
+        ) : (
+          <div ref={userCardRef}>
+            <button
+              className="hover:cursor-pointer flex items-center"
+              onClick={() => setShowUserCard(!showUserCard)}
+            >
+              <FaUser
+                className="hover:text-white"
+                aria-label="User Card"
+                size={32}
+              />
+            </button>
+            {showUserCard && <AuthMenu handleLogin={handleLogin} />}
+          </div>
+        )}
       </div>
-      <div ref={ref} className="sm:hidden">
+      <div ref={burgerRef} className="sm:hidden">
         <button className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
           <IoMenu aria-label="Menu" size={48} />
         </button>
@@ -68,12 +119,12 @@ const Navbar = () => {
                 <AiFillProduct />
                 Products
               </Link>
-              <hr />
+              <hr className="border-0 h-1 bg-surface" />
               <Link to="/Cart" aria-label="Cart" className="text-black p-5">
                 <FaShoppingCart />
                 Cart
               </Link>
-              <hr />
+              <hr className="border-0 h-1 bg-surface" />
               <Link
                 to="/Account"
                 aria-label="Account"
